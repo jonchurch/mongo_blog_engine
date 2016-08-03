62236c514baa7b698ee9eacc5097fe316b0c1767
Jon Church <me@jonchurch.com>
2016-08-03 19:09:41 -0400

    added user mongoose mode

---
+++ models/user.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	age: {
+		type: Number,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	}
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
