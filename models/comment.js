2952fb3f0dc4ad1e1830a1902df68ad03f6149d0
Jon Church <me@jonchurch.com>
2016-08-03 18:58:27 -0400

    added comment mongoose model

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 6 */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		reuqired: true
+	},
+	body: {
+		type: String,
+		required: true
+	},
+	created: {
+		type: Date,
+		required: true
+	},
+	post: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+	updated: {
+		type: Date,
+		required: true
+	}
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
