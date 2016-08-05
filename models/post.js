21102102e388194d37447c1c992bfd73152001d2
Jon Church <me@jonchurch.com>
2016-08-04 20:25:32 -0400

    added added DB interaction for createPost, getPosts, getPostById

--- models/post.js
+++ models/post.js
@@ -3,28 +3,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-	title: {
-		type: String,
-		required: true
-	},
-	body: {
-		type: String,
-		required: true
-	},
-	author: String,
-	/*author: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},*/
-	created: {
-		type: Date,
-		required: true
-	},
-	updated: {
-		type: Date,
-		required: true
-	}
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    author: String,
+    /*author: {
+    	type: mongoose.Schema.Types.ObjectId,
+    	ref: 'User',
+    	required: true
+    },*/
+    created: {
+        type: Date,
+        required: true
+    },
+    updated: {
+        type: Date,
+        required: true
+    }
+});
+
+postSchema.pre('findOneAndUpdate', function() {
+    this.update({}, {
+        $set: {
+            updated: new Date()
+        }
+    });
 });
 
 const Post = mongoose.model('Post', postSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -9,43 +9,74 @@ router.post('/posts', createPost);
 router.delete('/posts/:postId', deletePost);
 router.put('/posts/:postId', updatePost);
 
-function getAllPosts(req, res, next){
-	console.log('getting all of the posts');
-	next();
-}
-function getPostById(req, res, next){
-	
-}
-
-function createPost(req, res, next){
-	const post = new Post({
-		title: req.body.title,
-		author: req.body.author,
-		body: req.body.body,
-		created: new Date(),
-		updated: new Date()
-	});
-	post.save(function(err, result){
-		if (err) {
-			res.status(500).json({
-				msg: err
-			});
-		} else {
-			res.status(201).json({
-				post: result
-			});
-		}
-	});
+function getAllPosts(req, res, next) {
+    Post.find({}, function(err, foundPosts) {
+        if (err) {
+            res.status(500).json({
+                msg: err
+            });
+            return;
+        } else {
+            res.status(200).json({
+                posts: foundPosts
+            });
+        }
+    });
 }
 
-function deletePost(req, res, next){
-	console.log('deleting a post');
-	next();
+function getPostById(req, res, next) {
+	Post.findById(req.params.id, function(err, foundPost) {
+        if (err) {
+            res.status(500).json({
+                msg: err
+            });
+            return;
+        } else {
+            res.status(200).json({
+                posts: foundPost
+            });
+        }
+    });
 }
 
-function updatePost(req, res, next){
-	console.log('updating a post');
-	next();
+function createPost(req, res, next) {
+    const post = new Post({
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body,
+        created: new Date(),
+        updated: new Date()
+    });
+    post.save(function(err, result) {
+        if (err) {
+            res.status(500).json({
+                msg: err
+            });
+        } else {
+            res.status(201).json({
+                post: result
+            });
+        }
+    });
+}
+
+function deletePost(req, res, next) {
+    console.log('deleting a post');
+    next();
+}
+
+function updatePost(req, res, next) {
+    Post.findOneAndUpdate({_id: req.params.postId}, req.body, function(err, result){
+    	if (err) {
+    		res.status(500).json({ 
+    				msg: err
+    		});
+    	} else {
+    		res.status(200).json({
+    			oldPost: result
+    		});
+    	}
+    });
 }
 
 module.exports = router;
