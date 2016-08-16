3ff5d0b468d6f55c7c2c11fc8cf2e062fa9d7c74
Jon Church <me@jonchurch.com>
2016-08-16 19:02:48 -0400

    added password validation using salt and hash crypto functions

--- models/user.js
+++ models/user.js
@@ -1,26 +1,49 @@
 /*jshint esversion: 6 */
+'use strict';
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto')
 
 const userSchema = new Schema({
-	firstName: {
-		type: String,
-		required: true
-	},
-	lastName: {
-		type: String,
-		required: true
-	},
-	age: {
-		type: Number,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	}
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    hash: {
+        type: String,
+        required: false
+    },
+    salt: {
+        type: String,
+        required: false
+    }
 });
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex')
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+        .toString('hex')
+}
+userSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+        .toString('hex')
+    return this.hash === hash
+}
+
+userSchema.methods.generateJwt = function() {}
+
 userSchema.pre('findOneAndUpdate', function() {
     this.update({}, {
         $set: {
